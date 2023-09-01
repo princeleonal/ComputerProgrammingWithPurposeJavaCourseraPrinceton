5c5f99d6fe6161ee6416f032e9439ed8d3f0b48d
YagmurGULEC <yagmurgulec89@gmail.com>
2023-08-31 20:09:13 -0400

    README modified and grades added

--- Week4_exercises/RandomSeq.java
+++
@@ -1,8 +0,0 @@
-public class RandomSeq {
-    public static void main(String[] args) {
-        int N = Integer.parseInt(args[0]);
-        for (int i = 0; i < N; i++)
-            StdOut.println(1);
-
-    }
-}
